04e393ca119eb1bcdc8cf6a95ed8b9eb4fce4674
Samir Trajano Feitosa <samir.feitosa@virtus.ufcg.edu.br>
2019-10-10 22:59:56 -0300

    add future to car api

--- flutter_cars/lib/app/pages/home/home_page.dart
+++ flutter_cars/lib/app/pages/home/home_page.dart
@@ -14,57 +14,70 @@ class HomePage extends StatelessWidget {
   }
 
   _body() {
-    final List<Car> cars = CarApi.fetchCars();
+    final Future<List<Car>> carsFuture = CarApi.fetchCars();
 
-    return Container(
-      margin: EdgeInsets.all(16),
-      child: ListView.builder(
-        itemCount: cars.length,
-        itemBuilder: (context, index) {
-          final Car car = cars[index];
-          return Card(
-            color: Colors.grey[100],
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Center(
-                    child: Image.network(
-                      car.urlPhoto,
-                      width: 250,
-                    ),
-                  ),
-                  Text(
-                    car.name,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  Text(
-                    "description...",
-                    style: TextStyle(fontSize: 14),
-                  ),
-                  ButtonTheme.bar(
-                    child: ButtonBar(
-                      children: <Widget>[
-                        FlatButton(
-                          child: const Text('DETAILS'),
-                          onPressed: () {},
-                        ),
-                        FlatButton(
-                          child: const Text('SHARE'),
-                          onPressed: () {},
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
+    return FutureBuilder(
+      future: carsFuture,
+      builder: (context, snapshot) {
+        if(!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator(),);
+        }
+        final List<Car> cars = snapshot.data;
+        return _listView(cars);
+      },
     );
+  }
+
+  Container _listView(List<Car> cars) {
+    return Container(
+    margin: EdgeInsets.all(16),
+    child: ListView.builder(
+      itemCount: cars != null ? cars.length : 0,
+      itemBuilder: (context, index) {
+        final Car car = cars[index];
+        return Card(
+          color: Colors.grey[100],
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: Image.network(
+                    car.urlPhoto,
+                    width: 250,
+                  ),
+                ),
+                Text(
+                  car.name,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 25),
+                ),
+                Text(
+                  "description...",
+                  style: TextStyle(fontSize: 14),
+                ),
+                ButtonTheme.bar(
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('DETAILS'),
+                        onPressed: () {},
+                      ),
+                      FlatButton(
+                        child: const Text('SHARE'),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    ),
+  );
   }
 }
--- flutter_cars/lib/data/services/car_api.dart
+++ flutter_cars/lib/data/services/car_api.dart
@@ -2,9 +2,10 @@
 import 'package:flutter_cars/data/services/models/Car.dart';
 
 class CarApi {
-  static List<Car> fetchCars() {
+  static Future<List<Car>> fetchCars() async {
     final cars = List<Car>();
 
+    await Future.delayed(Duration(seconds: 2));
     cars.add(Car(name: "adillac Deville Convertible", urlPhoto: "http://www.livroandroid.com.br/livro/carros/classicos/Cadillac_Deville_Convertible.png"));
     cars.add(Car(name: "Chevrolet Bel-Air", urlPhoto: "http://www.livroandroid.com.br/livro/carros/classicos/Chevrolet_BelAir.png"));
     cars.add(Car(name: "Ferrari 250 GTO", urlPhoto: "http://www.livroandroid.com.br/livro/carros/classicos/Ferrari_250_GTO.png"));
