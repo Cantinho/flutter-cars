f17e7c1636d2998f6cda7be7545f36a06ae692b9
Samir Trajano Feitosa <samir.feitosa@virtus.ufcg.edu.br>
2019-10-12 16:11:56 -0300

    add shared preference; save tab index on shared preferences

--- flutter_cars/lib/app/pages/home/home_page.dart
+++ flutter_cars/lib/app/pages/home/home_page.dart
@@ -1,48 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cars/app/pages/home/cars_listview.dart';
 import 'package:flutter_cars/app/pages/home/drawer_list.dart';
+import 'package:flutter_cars/app/utils/prefs.dart';
 import 'package:flutter_cars/data/services/car_api.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin<HomePage> {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _initTabs();
+  }
+
+  _initTabs() async {
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.index = await Prefs.getInt(("tabIdx"));
+    _tabController.addListener(() {
+      Prefs.setInt("tabIdx", _tabController.index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Carros'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                text: "Classics",
-              ),
-              Tab(
-                text: "Sportives",
-              ),
-              Tab(
-                text: "Lux",
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CarsListView(
-              carType: CarType.classic,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Carros'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(
+              text: "Classics",
             ),
-            CarsListView(carType: CarType.sport),
-            CarsListView(
-              carType: CarType.lux,
+            Tab(
+              text: "Sportives",
+            ),
+            Tab(
+              text: "Lux",
             ),
           ],
         ),
-        drawer: DrawerList(),
       ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          CarsListView(
+            carType: CarType.classic,
+          ),
+          CarsListView(carType: CarType.sport),
+          CarsListView(
+            carType: CarType.lux,
+          ),
+        ],
+      ),
+      drawer: DrawerList(),
     );
   }
 }
